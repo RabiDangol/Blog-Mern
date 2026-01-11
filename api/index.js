8bbee4828ade292169dafd32e5ab5d0e2a778524
Rabi Dangol <dangolrabi79@gmail.com>
2026-01-11 21:39:03 +0545

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connection successfully...");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
