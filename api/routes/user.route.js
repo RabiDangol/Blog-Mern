cec4f1d095a18e3d38465bad2a631b011a1390d5
Rabi Dangol <dangolrabi79@gmail.com>
2026-08-16 21:23:46 +0545

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,15 +4,21 @@ import {
   test,
   uploadProfileImage,
   updateUser,
+  deleteUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+
+//update the profile data
 router.put("/update/:userId", verifyToken, updateUser);
 
 // Upload profile image
 router.post("/upload", upload.single("image"), uploadProfileImage);
+
+//delete the account
+router.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default router;
